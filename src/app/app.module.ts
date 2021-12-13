1d0f50b86a0d9df206f3a228852f468a64ead90c
Salah Eddine Akhrif <salaheddineakhrif@gmail.com>
2021-12-13 10:58:14 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './compenents/header/header.component';
+import { ButtonComponent } from './compenents/button/button.component';
+import { TasksComponent } from './compenents/tasks/tasks.component';
+import { TaskItemComponent } from './compenents/task-item/task-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AddTaskComponent } from './compenents/add-task/add-task.component';
+import { AboutComponent } from './compenents/about/about.component';
+import { FooterComponent } from './compenents/footer/footer.component';
+
+
+const appRoutes: Routes = [
+  {path: '', component: TasksComponent},
+  {path: 'about', component: AboutComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent,
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing:true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock.tasks.ts
@@ -0,0 +1,21 @@
+import {Task} from './Task'
+
+export const TASKS: Task[]= [{
+    id: 1,
+    text: 'Doctor appointment', 
+    day: 'May 6 2021', 
+    reminder: true,
+    
+},
+{
+    id: 2,
+    text: 'gym time', 
+    day: 'June 9 2021', 
+    reminder: true, 
+},
+{
+    id: 3,
+    text: 'Food shoping', 
+    day: 'May 7 2021', 
+    reminder: false,
+},]
